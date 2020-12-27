b611cf94ca1c1e087e6e1d9c893bfeaf12ca2232
Elijah Pekson <elijah.pekson@twala.io>
2020-12-27 18:24:35 +0800

    Adding transfer on behalf feature + adding logging for transfer and approval events

--- MyToken.sol
+++ MyToken.sol
@@ -5,11 +5,15 @@ pragma solidity ^0.7.4;
 
 contract MyToken {
     mapping (address => uint256) private _balances;
+    mapping (address => mapping(address => uint256)) private _allowances;
 
     string private _name;
     string private _symbol;
     uint8 private _decimals;
     uint256 private _totalSupply;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     constructor(uint256 initialSupply, string memory tokenName, string memory tokenSymbol, uint8 decimalUnits) public {
         _balances[msg.sender] = initialSupply;
@@ -47,13 +51,50 @@ contract MyToken {
         _balances[account] = balance;
     }
 
+    function allowance (address owner, address spender) public view returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function setAllowance(address owner, address spender, uint256 amount) internal {
+        _allowances[owner][spender] = amount;
+    }
+
     function transfer(address beneficiary, uint256 amount) public returns (bool) {
         require(beneficiary != address(0), "Beneficiary address cannot be zero.");
         require(_balances[msg.sender] >= amount, "Send does not have enough balance.");
         // 0- 255 only (integer overflow)
         require(_balances[beneficiary] + amount > _balances[beneficiary], "Addition overflow");
+
         _balances[msg.sender] -= amount;
         _balances[beneficiary] += amount;
+
+        // emit an event inside function that will generate a log message
+        emit Transfer(msg.sender, beneficiary, amount);
+
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool success) {
+        require(spender != address(0), "Spender address cannot be zero.");
+        _allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address beneficiary, uint256 amount) public returns (bool) {
+        require(sender != address(0), "Beneficiary address cannot be zero.");
+        require(beneficiary != address(0), "Beneficiary address cannot be zero.");
+        require(amount <= _allowances[sender][msg.sender], "Allowance is not enough");
+        require(_balances[sender] >= amount, "Send does not have enough balance.");
+        // 0- 255 only (integer overflow)
+        require(_balances[beneficiary] + amount > _balances[beneficiary], "Addition overflow");
+
+        _balances[sender] -= amount;
+        _allowances[sender][msg.sender] -= amount;
+        _balances[beneficiary] += amount;
+
+        emit Transfer(sender, beneficiary, amount);
+
         return true;
     }
 }
